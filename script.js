eeed88e161f9330e6d822b0e1c9979f49a48b807
ComfyClouds <iibow.v2@gmail.com>
2026-05-31 22:42:22 +0300

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   fianceName: "Mohanad",
-  relationshipStartDate: "2025-08-22T00:00:00",
+  relationshipStartDate: "2021-08-28T00:00:00",
   googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbxiVbmKvc924DMI39FAe2NhXDNBAYdNEWxUhvzPYXFcuvK9Rss6ZaQb5Rjhik3wFiQ3RA/exec",
   voiceMessageUrl: "https://www.dropbox.com/scl/fi/vohfx3nxg2xu4d5adsdqx/AUD-20260531-WA0039.m4a?rlkey=vr3eaoz59u4acl27xukeb8enp&st=k5vhoejs&raw=1",
   finaleSongUrl: "kol-sana.mp3",
